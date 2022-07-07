471872b6b9cc1d50c879f72dc3fc08ed519e6b9f
Nicole Gay <nicolerg@stanford.edu>
2022-07-06 17:35:10 -0700

    start data doc

---
+++ R/data.R
@@ -0,0 +1,294 @@
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 111480 rows and 20 variables:
+#' \describe{
+#'   \item{\code{feature_ID}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{tissue}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{sex}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_group}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{assay}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{covariates}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{removed_samples}}{logical COLUMN_DESCRIPTION}
+#'   \item{\code{logFC}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{logFC_se}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{shrunk_logFC}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{shrunk_logFC_se}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{zscore}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_average_intensity_se}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{reference_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{reference_average_intensity_se}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{adj_p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{tissue_abbreviation}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{selection_fdr}}{double COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+#' @name TRNSCRPT_DA
+"TRNSCRPT_BLOOD_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_HIPPOC_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_CORTEX_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_HYPOTH_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_SKMGN_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_SKMVL_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_HEART_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_KIDNEY_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_ADRNL_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_COLON_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_SPLEEN_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_TESTES_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_OVARY_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_VENACV_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_LUNG_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_SMLINT_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_LIVER_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_BAT_DA"
+
+#' @rdname TRNSCRPT_DA
+"TRNSCRPT_WATSC_DA"
+
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 88864 rows and 16 variables:
+#' \describe{
+#'   \item{\code{feature_ID}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{tissue}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{assay}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{sex}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{logFC_se}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{logFC}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{tscore}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{covariates}}{logical COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_group}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{reference_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{numNAs}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{adj_p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{tissue_abbreviation}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{selection_fdr}}{double COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"PROT_CORTEX_DA"
+
+PROT_CORTEX_DA
+PROT_SKMGN_DA
+PROT_HEART_DA
+PROT_KIDNEY_DA
+PROT_LUNG_DA
+PROT_LIVER_DA
+PROT_WATSC_DA
+
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 372016 rows and 16 variables:
+#' \describe{
+#'   \item{\code{feature_ID}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{tissue}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{assay}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{sex}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{logFC_se}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{logFC}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{tscore}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{covariates}}{logical COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_group}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{reference_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{numNAs}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{adj_p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{tissue_abbreviation}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{selection_fdr}}{double COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"PHOSPHO_CORTEX_DA"
+
+PHOSPHO_CORTEX_DA
+PHOSPHO_SKMGN_DA
+PHOSPHO_HEART_DA
+PHOSPHO_KIDNEY_DA
+PHOSPHO_LUNG_DA
+PHOSPHO_LIVER_DA
+PHOSPHO_WATSC_DA
+
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 41704 rows and 16 variables:
+#' \describe{
+#'   \item{\code{feature_ID}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{tissue}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{assay}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{sex}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{logFC_se}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{logFC}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{tscore}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{covariates}}{logical COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_group}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{reference_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{numNAs}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{adj_p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{tissue_abbreviation}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{selection_fdr}}{double COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"ACETYL_HEART_DA"
+
+ACETYL_HEART_DA
+ACETYL_LIVER_DA
+
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 56624 rows and 16 variables:
+#' \describe{
+#'   \item{\code{feature_ID}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{tissue}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{assay}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{sex}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{logFC_se}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{logFC}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{tscore}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{covariates}}{logical COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_group}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{reference_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{numNAs}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{adj_p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{tissue_abbreviation}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{selection_fdr}}{double COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"UBIQ_HEART_DA"
+
+UBIQ_HEART_DA
+UBIQ_LIVER_DA
+
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 10144 rows and 25 variables:
+#' \describe{
+#'   \item{\code{feature_ID}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{tissue}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{dataset}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{assay}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{sex}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{is_targeted}}{logical COLUMN_DESCRIPTION}
+#'   \item{\code{site}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{metabolite}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{metabolite_refmet}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{logFC}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{logFC_se}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{tscore}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{cv}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{control_cv}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{covariates}}{logical COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_group}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{reference_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{tissue_abbreviation}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{mz}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{rt}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{neutral_mass}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{adj_p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{selection_fdr}}{double COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"METAB_PLASMA_DA"
+
+METAB_PLASMA_DA
+METAB_HIPPOC_DA
+METAB_CORTEX_DA
+METAB_HYPOTH_DA
+METAB_SKMGN_DA
+METAB_SKMVL_DA
+METAB_HEART_DA
+METAB_KIDNEY_DA
+METAB_ADRNL_DA
+METAB_COLON_DA
+METAB_SPLEEN_DA
+METAB_TESTES_DA
+METAB_OVARY_DA
+METAB_VENACV_DA
+METAB_LUNG_DA
+METAB_SMLINT_DA
+METAB_LIVER_DA
+METAB_BAT_DA
+METAB_WATSC_DA
+
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 480 rows and 15 variables:
+#' \describe{
+#'   \item{\code{feature_ID}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{panel}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{tissue}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{assay}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_group}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{logFC}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{logFC_se}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{p_value}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{reference_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{comparison_average_intensity}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{sex}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{covariates}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{adj_p_value}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{tissue_abbreviation}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{selection_fdr}}{double COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"IMMUNO_PLASMA_DA"
+
+IMMUNO_PLASMA_DA
+IMMUNO_HIPPOC_DA
+IMMUNO_CORTEX_DA
+IMMUNO_ADRNL_DA
+IMMUNO_TESTES_DA
+IMMUNO_OVARY_DA
+IMMUNO_SKMGN_DA
+IMMUNO_BAT_DA
+IMMUNO_WATSC_DA
+IMMUNO_COLON_DA
+IMMUNO_SMLINT_DA
+IMMUNO_LIVER_DA
+IMMUNO_SKMVL_DA
+IMMUNO_HEART_DA
+IMMUNO_KIDNEY_DA
+IMMUNO_SPLEEN_DA
+IMMUNO_LUNG_DA
