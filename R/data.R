3265ec4242dbfee7117c118029f067df8fc480b2
Nicole Gay <nicolerg@stanford.edu>
2022-08-10 10:23:25 -0700

    fix some docs

--- R/data.R
+++ R/data.R
@@ -155,9 +155,9 @@
 #'   \item{\code{dataset}}{@eval dataset()}
 #'   \item{\code{selection_fdr}}{@eval selection_fdr()}
 #'   \item{\code{metabolite_refmet}}{character, RefMet name of metabolite}
-#'   \item{\code{feature}}{character, duplicated \code{feature} in the format \code{[ASSAY_ABBREV];[TISSUE_ABBREV];[feature_ID]}}
+#'   \item{\code{feature}}{character, duplicated \code{feature} in the format \code{\link{ASSAY_ABBREV};\link{TISSUE_ABBREV};[feature_ID]}}
 #'   \item{\code{new_feature_ID}}{character, new unique \code{feature_ID} with \code{panel} or \code{dataset} prepended}
-#'   \item{\code{new_feature}}{character, new unique \code{feature} in the format \code{[ASSAY_ABBREV];[TISSUE_ABBREV];[new_feature_ID]}} 
+#'   \item{\code{new_feature}}{character, new unique \code{feature} in the format \code{\link{ASSAY_ABBREV};\link{TISSUE_ABBREV};[new_feature_ID]}} 
 #'}
 #' @details 91 IMMUNO and METAB features were measured on multiple platforms and have multiple differential analysis results. 
 #'     In order to distinguish between the different sets of results, these 
@@ -1276,7 +1276,7 @@ NULL
 #'   \item{\code{tissue}}{@eval tissue()}
 #'   \item{\code{tissue_code}}{@eval tissue_code()}
 #'   \item{\code{assay}}{@eval assay()}
-#'   \item{\code{assay_code}}{@eval assay_code()}
+#'   \item{\code{assay_code}}{@eval}
 #'   \item{\code{platform}}{character, LUMINEX panel if \code{assay} is IMMUNO}
 #'   \item{\code{pid}}{integer, participant ID, one per animal}
 #'   \item{\code{group}}{character, combination of sex and training time point that the sample belongs to, e.g., "female_1w"}
